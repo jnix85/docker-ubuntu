944a3f4e1a98611415b00071f8d65d7bff936818
Jason Parks <>
2023-10-30 17:46:43 -0500

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 for i in jammy lunar mantic
 do git checkout $i
-docker buildx build --platform linux/amd64,linux/arm64 --pull --push -t registry.pupgizmo.com/ubuntu/ubuntu:${i} -f Dockerfile . 
+docker buildx build --platform linux/amd64,linux/arm64 --pull --push -t registry.pupgizmo.com/ubuntu/${i}:latest -f Dockerfile . 
 done
 git checkout main
